7ede9c61727a3e8f8cf27f060b92a2a1682fb129
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-10-02 17:06:47 -0400

    Search rewrite: Initial PQF driver for QueryParser

---
+++ QueryParser/PQF.pm
@@ -0,0 +1,108 @@
+package QueryParser::PQF;
+use base 'QueryParser';
+
+sub bib1_field_map {
+    my ($self, $map) = @_;
+
+    $self->custom_data->{bib1_field_map} ||= {};
+    $self->custom_data->{bib1_field_map} = $map if ($map);
+    return $self->custom_data->{bib1_field_map};
+}
+
+sub add_bib1_field_map {
+    my ($self, $class, $field, $server, $attributes) = @_;
+
+    my $use_attr1 = $attributes->{1};
+    $self->add_search_field( $class => $field );
+    $self->bib1_field_map->{$server}{'by_class'}{$class}{$field} = $attributes;
+    $self->bib1_field_map->{$server}{'by_attr'}{$use_attr1} = { 'classname' => $class, 'field' => $field } if $use_attr1;
+
+    return $self->bib1_field_map;
+}
+
+sub bib1_field_by_attr {
+    my ($self, $server, $attr) = @_;
+
+    return unless ($server && $attr);
+
+    return $self->bib1_field_map->{$server}{'by_attr'}{$attr};
+}
+
+sub bib1_field_by_class {
+    my ($self, $server, $class, $field) = @_;
+
+    return unless ($server && $class);
+
+    return $self->bib1_field_map->{$server}{'by_class'}{$class}{$field};
+}
+
+sub target_syntax {
+    my ($self, $server, $query) = @_;
+    my $pqf = '';
+    $self->parse($query) if $query;
+    return $self->parse_tree->target_syntax($server);
+}
+
+#-------------------------------
+package QueryParser::PQF::query_plan;
+use base 'QueryParser::query_plan';
+
+sub target_syntax {
+    my ($self, $server) = @_;
+    my $pqf = '';
+    my $node_count = 0;
+
+    for my $node ( @{$self->query_nodes} ) {
+
+        if (ref($node)) {
+            $node_count++;
+            $pqf .= $node->target_syntax($server);
+        }
+    }
+    $pqf = ($self->joiner eq '|' ? ' @or ' : ' @and ') . $pqf if ($node_count > 1);
+    return $pqf;
+}
+
+#-------------------------------
+package QueryParser::PQF::query_plan::filter;
+use base 'QueryParser::query_plan::filter';
+
+#-------------------------------
+package QueryParser::PQF::query_plan::facet;
+use base 'QueryParser::query_plan::facet';
+
+#-------------------------------
+package QueryParser::PQF::query_plan::modifier;
+use base 'QueryParser::query_plan::modifier';
+
+#-------------------------------
+package QueryParser::PQF::query_plan::node::atom;
+use base 'QueryParser::query_plan::node::atom';
+
+sub target_syntax {
+    my ($self, $server) = @_;
+    my $pqf = '';
+
+    return ' "' .  $self->content . '" ';
+}
+
+#-------------------------------
+package QueryParser::PQF::query_plan::node;
+use base 'QueryParser::query_plan::node';
+
+sub target_syntax {
+    my ($self, $server) = @_;
+    my $pqf = '';
+    my $atom_count = 0;
+
+    foreach my $atom (@{$self->query_atoms}) {
+        if (ref($atom)) {
+            $atom_count++;
+            $pqf .= $atom->target_syntax($server);
+        }
+    }
+    $pqf = (QueryParser::_util::default_joiner eq '|' ? ' @or ' : ' @and ') . $pqf if ($atom_count > 1);
+    return $pqf;
+}
+
+1;
