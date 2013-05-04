3947c55d70ad6c71f0db82fc50842a48d5f99d7b
Jared Camins-Esakov <jcamins@cpbibliography.com>
2013-05-04 18:50:32 -0400

    Add offline circ settings

--- circ/offlinesync.pl
+++ circ/offlinesync.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/perl
 
+# Copyright 2013 C & P Bibliography Services
+#
 # This file is part of Koha.
 #
 # Koha is free software; you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
+# Foundation; either version 3 of the License, or (at your option) any later
 # version.
 #
 # Koha is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -25,46 +27,65 @@ use C4::Context;
 use C4::Koha;
 
 my $query = new CGI;
-my ($template, $loggedinuser, $cookie, $flags)
-= get_template_and_user({template_name => "circ/offlinesync.tt",
-				query => $query,
-				type => "intranet",
-				authnotrequired => 0,
-				flagsrequired => {circulate => "circulate_remaining_permissions"},
-				});
+my ( $template, $loggedinuser, $cookie, $flags ) = get_template_and_user(
+    {
+        template_name   => "circ/offlinesync.tt",
+        query           => $query,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { circulate => "circulate_remaining_permissions" },
+    }
+);
 
-# Checking if there is a Fast Cataloging Framework
+my $page     = $query->param('page') || 0;
+my $startrec = int($page) * 5000;
+my $req_data = $query->param('data') || '';
 
-my $dbh = C4::Context->dbh;
-my $sth;
-my $sql;
-
-my $page = $query->param('page');
-my $startrec = int($page) * 3000;
-
-if ($query->param('data') eq 'all') {
+if ( $req_data eq 'all' ) {
     print $query->header;
-    print to_json( {
-                     'patrons' => get_data("SELECT borrowers.borrowernumber, cardnumber, surname, firstname, title, othernames, initials, streetnumber, streettype, address, address2, city, state, zipcode, country, email, phone, mobile, fax, dateofbirth, branchcode, categorycode, dateenrolled, dateexpiry, gonenoaddress, lost, debarred, debarredcomment, SUM(accountlines.amountoutstanding) AS fine FROM borrowers LEFT JOIN accountlines ON borrowers.borrowernumber=accountlines.borrowernumber GROUP BY borrowers.borrowernumber LIMIT $startrec, 5000;", 'cardnumber'),
-                     # NOTE: we can't fit very long titles on the interface so there isn't really any point in transferring them
-                     'items' => get_data("SELECT items.barcode AS barcode, items.itemnumber AS itemnumber, items.itemcallnumber AS callnumber, items.homebranch AS homebranch, items.holdingbranch AS holdingbranch, items.itype AS itemtype, LEFT(biblio.title, 60) AS title, biblio.author AS author, biblio.biblionumber AS biblionumber FROM items JOIN biblio ON biblio.biblionumber = items.biblionumber LIMIT $startrec, 5000;", 'barcode'),
-                   } );
-} elsif ($query->param('data') eq 'patrons') {
+    print to_json(
+        {
+            'patrons' => get_data(
+"SELECT borrowers.borrowernumber, cardnumber, surname, firstname, title, othernames, initials, streetnumber, streettype, address, address2, city, state, zipcode, country, email, phone, mobile, fax, dateofbirth, branchcode, categorycode, dateenrolled, dateexpiry, gonenoaddress, lost, debarred, debarredcomment, SUM(accountlines.amountoutstanding) AS fine FROM borrowers LEFT JOIN accountlines ON borrowers.borrowernumber=accountlines.borrowernumber GROUP BY borrowers.borrowernumber LIMIT $startrec, 5000;",
+                'cardnumber'
+            ),
+
+# NOTE: we can't fit very long titles on the interface so there isn't really any point in transferring them
+            'items' => get_data(
+"SELECT items.barcode AS barcode, items.itemnumber AS itemnumber, items.itemcallnumber AS callnumber, items.homebranch AS homebranch, items.holdingbranch AS holdingbranch, items.itype AS itemtype, LEFT(biblio.title, 60) AS title, biblio.author AS author, biblio.biblionumber AS biblionumber FROM items JOIN biblio ON biblio.biblionumber = items.biblionumber LIMIT $startrec, 5000;",
+                'barcode'
+            ),
+        }
+    );
+}
+elsif ( $req_data eq 'patrons' ) {
     print $query->header;
-    print to_json( {
-                     'patrons' => get_data("SELECT borrowers.borrowernumber, cardnumber, surname, firstname, title, othernames, initials, streetnumber, streettype, address, address2, city, state, zipcode, country, email, phone, mobile, fax, dateofbirth, branchcode, categorycode, dateenrolled, dateexpiry, gonenoaddress, lost, debarred, debarredcomment, SUM(accountlines.amountoutstanding) AS fine FROM borrowers LEFT JOIN accountlines ON borrowers.borrowernumber=accountlines.borrowernumber GROUP BY borrowers.borrowernumber;", 'cardnumber')
-                    } );
-} elsif ($query->param('data') eq 'items') {
+    print to_json(
+        {
+            'patrons' => get_data(
+"SELECT borrowers.borrowernumber, cardnumber, surname, firstname, title, othernames, initials, streetnumber, streettype, address, address2, city, state, zipcode, country, email, phone, mobile, fax, dateofbirth, branchcode, categorycode, dateenrolled, dateexpiry, gonenoaddress, lost, debarred, debarredcomment, SUM(accountlines.amountoutstanding) AS fine FROM borrowers LEFT JOIN accountlines ON borrowers.borrowernumber=accountlines.borrowernumber GROUP BY borrowers.borrowernumber;",
+                'cardnumber'
+            )
+        }
+    );
+}
+elsif ( $req_data eq 'items' ) {
     print $query->header;
-    print to_json( {
-                     'items' => get_data("SELECT items.barcode AS barcode, items.itemnumber AS itemnumber, items.itemcallnumber AS callnumber, items.homebranch AS homebranch, items.holdingbranch AS holdingbranch, items.itype AS itemtype, biblio.title AS title, biblio.author AS author, biblio.biblionumber AS biblionumber FROM items JOIN biblio ON biblio.biblionumber = items.biblionumber;", 'barcode'),
-                   } );
-} else {
+    print to_json(
+        {
+            'items' => get_data(
+"SELECT items.barcode AS barcode, items.itemnumber AS itemnumber, items.itemcallnumber AS callnumber, items.homebranch AS homebranch, items.holdingbranch AS holdingbranch, items.itype AS itemtype, biblio.title AS title, biblio.author AS author, biblio.biblionumber AS biblionumber FROM items JOIN biblio ON biblio.biblionumber = items.biblionumber;",
+                'barcode'
+            ),
+        }
+    );
+}
+else {
     output_html_with_http_headers $query, $cookie, $template->output;
 }
 
 sub get_data {
-    my ($sql, $key) = @_;
+    my ( $sql, $key ) = @_;
     my $dbh = C4::Context->dbh;
     my $sth = $dbh->prepare($sql);
     $sth->execute();
--- koha-tmpl/intranet-tmpl/prog/en/js/offlinecirc.js
+++ koha-tmpl/intranet-tmpl/prog/en/js/offlinecirc.js
@@ -1,4 +1,23 @@
+/* Copyright 2013 C & P Bibliography Services
+ *
+ * This file is part of Koha.
+ *
+ * Koha is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Koha; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 (function( kohadb, $, undefined ) {
+    kohadb.settings = kohadb.settings || {};
     kohadb.initialize = function (callback) {
         $.indexedDB("koha", {
             "version": 1,
@@ -13,15 +32,34 @@
                     var styles = versionTransaction.createObjectStore("issues", {
                         "keyPath": "barcode"
                     });
-                    var styles = versionTransaction.createObjectStore("transactions", {
-                        "keyPath": "barcode"
+                    var transactions = versionTransaction.createObjectStore("transactions", {
+                        "keyPath": "timestamp"
+                    });
+                    var settings = versionTransaction.createObjectStore("offline_settings", {
+                        "keyPath": "key"
                     });
                 },
             }
         }).done(function(){
             if (typeof callback === 'function') {
                 callback();
+                kohadb.loadSetting('userid');
+                kohadb.loadSetting('branchcode');
+                kohadb.loadSetting('branchcode');
             }
         });
     };
+    kohadb.loadSetting = function (key) {
+        $.indexedDB("koha").transaction(["offline_settings"]).then(function(){
+        }, function(err, e){
+        }, function(transaction){
+            var settings = transaction.objectStore("offline_settings");
+            settings.get(key).done(function (item, error) {
+                if (typeof item !== 'undefined') {
+                    kohadb.settings[key] = item.value;
+                }
+            });
+        });
+    };
 }( window.kohadb = window.bndb || {}, jQuery ));
+
